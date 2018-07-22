f570f7a93d0c97a2d08b74ff58c7d164ec9c8d95
Spencer Miller <spence.miller@gmail.com>
2018-07-22 02:04:21 -0700

    tabs to spaces

--- MyVec.h
+++ MyVec.h
@@ -26,100 +26,100 @@ template
 class MyVec
 {
 public:
-	MyVec()
-	{
-	}
+    MyVec()
+    {
+    }
 
-	~MyVec()
-	{
-		Clear();
-		free(mRawBuffer);
-	}
+    ~MyVec()
+    {
+        Clear();
+        free(mRawBuffer);
+    }
 
-	size_t Capacity() const { return mCapacity; }
-	size_t Size() const { return mSize; }
+    size_t Capacity() const { return mCapacity; }
+    size_t Size() const { return mSize; }
 
-	void Append(const T& element)
-	{
-		if (FreeSpace() == 0)
-		{
-			Grow();
-		}
+    void Append(const T& element)
+    {
+        if (FreeSpace() == 0)
+        {
+            Grow();
+        }
 
-		new (&(mBuffer[mSize++])) T(element);
-	}
+        new (&(mBuffer[mSize++])) T(element);
+    }
 
-	void Clear()
-	{
-		DestroyElements();
-		mSize = 0;
-	}
+    void Clear()
+    {
+        DestroyElements();
+        mSize = 0;
+    }
 
-	void DestroyElements()
-	{
-		for (size_t i = 0; i < Size(); ++i)
-		{
-			mBuffer[i].~T();
-		}
-	}
+    void DestroyElements()
+    {
+        for (size_t i = 0; i < Size(); ++i)
+        {
+            mBuffer[i].~T();
+        }
+    }
 
-	void Reserve(size_t requested)
-	{
-		if (requested < mCapacity)
-		{
-			return;
-		}
+    void Reserve(size_t requested)
+    {
+        if (requested < mCapacity)
+        {
+            return;
+        }
 
-		const size_t requiredAlignment = alignof(T);
-		void* tmp = malloc(sizeof(T) * requested + requiredAlignment /*wiggle bytes*/);
+        const size_t requiredAlignment = alignof(T);
+        void* tmp = malloc(sizeof(T) * requested + requiredAlignment /*wiggle bytes*/);
 
-		// with help from Game Engine Architecture (5.2.1.3 Aligned Allocations)
-		// ---------------------------------------------------------------------
-		// alignment will be some power of two, so subtract 1 to get the misalignment mask
-		const size_t misalignmentMask = requiredAlignment - 1; 
-		// if we AND the actual address with our mask, we'll see how many bytes off we are
-		const size_t misalignment     = reinterpret_cast<uintptr_t>(tmp) & misalignmentMask;
-		const size_t requiredOffset   = requiredAlignment - misalignment;
+        // with help from Game Engine Architecture (5.2.1.3 Aligned Allocations)
+        // ---------------------------------------------------------------------
+        // alignment will be some power of two, so subtract 1 to get the misalignment mask
+        const size_t misalignmentMask = requiredAlignment - 1; 
+        // if we AND the actual address with our mask, we'll see how many bytes off we are
+        const size_t misalignment     = reinterpret_cast<uintptr_t>(tmp) & misalignmentMask;
+        const size_t requiredOffset   = requiredAlignment - misalignment;
 
-		T* typedIntermediateBuffer = reinterpret_cast<T*>(reinterpret_cast<char*>(tmp) + requiredOffset);
+        T* typedIntermediateBuffer = reinterpret_cast<T*>(reinterpret_cast<char*>(tmp) + requiredOffset);
 
-		// Copy-Construct into the new buffer
-		// TODO do we have to worry about memory overlap?
-		for (size_t i = 0; i < Size(); ++i)
-		{
-			new (&(typedIntermediateBuffer[i])) T(mBuffer[i]);
-		}
+        // Copy-Construct into the new buffer
+        // TODO do we have to worry about memory overlap?
+        for (size_t i = 0; i < Size(); ++i)
+        {
+            new (&(typedIntermediateBuffer[i])) T(mBuffer[i]);
+        }
 
-		// kill off the old
-		DestroyElements();		
-		free(mRawBuffer);
+        // kill off the old
+        DestroyElements();        
+        free(mRawBuffer);
 
-		mBuffer    = typedIntermediateBuffer;
-		mRawBuffer = tmp;
-		mCapacity  = requested;
-	}
+        mBuffer    = typedIntermediateBuffer;
+        mRawBuffer = tmp;
+        mCapacity  = requested;
+    }
 
-	void Grow()
-	{
-		if (mCapacity == 0)
-		{
-			Reserve(1);
-		}
-		else
-		{
-			Reserve(mCapacity * 2);
-		}
-	}
+    void Grow()
+    {
+        if (mCapacity == 0)
+        {
+            Reserve(1);
+        }
+        else
+        {
+            Reserve(mCapacity * 2);
+        }
+    }
 
-	size_t FreeSpace() const { return mCapacity - mSize; }
+    size_t FreeSpace() const { return mCapacity - mSize; }
 
-	const T& operator[](int idx) const { return mBuffer[idx]; }
-	      T& operator[](int idx)       { return mBuffer[idx]; }
+    const T& operator[](int idx) const { return mBuffer[idx]; }
+          T& operator[](int idx)       { return mBuffer[idx]; }
 
 private:
-	T*     mBuffer    = nullptr;
-	void*  mRawBuffer = nullptr;
+    T*     mBuffer    = nullptr;
+    void*  mRawBuffer = nullptr;
 
-	size_t mSize      = 0;
-	size_t mCapacity  = 0;
+    size_t mSize      = 0;
+    size_t mCapacity  = 0;
 };
--- test.cpp
+++ test.cpp
@@ -7,42 +7,42 @@
 class DestructoType
 {
 public:
-	static size_t destruction_count;
+    static size_t destruction_count;
 
-	DestructoType() = default;
-	~DestructoType() { destruction_count++; }
+    DestructoType() = default;
+    ~DestructoType() { destruction_count++; }
 };
 size_t DestructoType::destruction_count = 0; // todo why does this have to be initialized?
 
 struct ThreeIntType
 {
-	int a, b, c;
+    int a, b, c;
 };
 
 struct PaddedType
 {
-	char a;
-	double b;
+    char a;
+    double b;
 };
 
 class Pigeon; // fwd
 class PigeonManager
 {
 public:
-	PigeonManager() = default;
-	static void RegisterPigeon(Pigeon* p) { mPigeons.insert(p); }
-	static void DeregisterPigeon(Pigeon* p) { mPigeons.erase(p); }
-	static bool IsRegistered(Pigeon* p) { return mPigeons.find(p) != mPigeons.end(); }
-	static std::set<Pigeon*> mPigeons;
+    PigeonManager() = default;
+    static void RegisterPigeon(Pigeon* p) { mPigeons.insert(p); }
+    static void DeregisterPigeon(Pigeon* p) { mPigeons.erase(p); }
+    static bool IsRegistered(Pigeon* p) { return mPigeons.find(p) != mPigeons.end(); }
+    static std::set<Pigeon*> mPigeons;
 };
 std::set<Pigeon*> PigeonManager::mPigeons;
 
 class Pigeon
 {
 public:
-	Pigeon() { PigeonManager::RegisterPigeon(this); }
-	Pigeon(const Pigeon& other) { PigeonManager::RegisterPigeon(this); }
-	~Pigeon() { PigeonManager::DeregisterPigeon(this); }
+    Pigeon() { PigeonManager::RegisterPigeon(this); }
+    Pigeon(const Pigeon& other) { PigeonManager::RegisterPigeon(this); }
+    ~Pigeon() { PigeonManager::DeregisterPigeon(this); }
 };
 
 // Fixtures
@@ -50,143 +50,143 @@ public:
 class MyVecTest : public ::testing::Test
 {
 protected:
-	virtual void SetUp() override {
-		DestructoType::destruction_count = 0;
-	}
-	virtual void TearDown() override {
-		PigeonManager::mPigeons.clear();
-	}
+    virtual void SetUp() override {
+        DestructoType::destruction_count = 0;
+    }
+    virtual void TearDown() override {
+        PigeonManager::mPigeons.clear();
+    }
 
-	MyVec<int> v;
-	MyVec<char> vc;
-	MyVec<float> vf;
-	MyVec<double> vd;
-	MyVec<ThreeIntType> v3i;
-	MyVec<PaddedType> vp;
-	MyVec<Pigeon> vpigeons;
+    MyVec<int> v;
+    MyVec<char> vc;
+    MyVec<float> vf;
+    MyVec<double> vd;
+    MyVec<ThreeIntType> v3i;
+    MyVec<PaddedType> vp;
+    MyVec<Pigeon> vpigeons;
 };
 
 // Tests
 // --------------------------------------------------------------------
 TEST_F(MyVecTest, Init)
 {
-	EXPECT_EQ(v.Size(), 0);
-	EXPECT_EQ(v.Capacity(), 0);
+    EXPECT_EQ(v.Size(), 0);
+    EXPECT_EQ(v.Capacity(), 0);
 }
 
 TEST_F(MyVecTest, Append)
 {
-	static const size_t insertions = 0xFFFF;
+    static const size_t insertions = 0xFFFF;
 
-	for (size_t i = 0; i < insertions; ++i)
-	{
-		v.Append(i);
-		// test that the size is correctly incrementing
-		EXPECT_EQ(v.Size(), i + 1);
-		// test that capacity is doubling when it should
-		EXPECT_EQ(std::log2(v.Capacity()), std::ceil(std::log2(i+1)));
-	}
+    for (size_t i = 0; i < insertions; ++i)
+    {
+        v.Append(i);
+        // test that the size is correctly incrementing
+        EXPECT_EQ(v.Size(), i + 1);
+        // test that capacity is doubling when it should
+        EXPECT_EQ(std::log2(v.Capacity()), std::ceil(std::log2(i+1)));
+    }
 
-	for (size_t i = 0; i < insertions; ++i)
-	{
-		// test that the values are in the correct positions
-		EXPECT_EQ(v[i], i);
-	}
+    for (size_t i = 0; i < insertions; ++i)
+    {
+        // test that the values are in the correct positions
+        EXPECT_EQ(v[i], i);
+    }
 }
 
 TEST_F(MyVecTest, Clear)
 {
-	MyVec<DestructoType> destructo;
-	
-	const size_t expected = 100;
-	destructo.Reserve(expected);
-	for (size_t i = 0; i < expected; ++i)
-	{
-		// todo emplace?
-		destructo.Append(DestructoType());
-	}
+    MyVec<DestructoType> destructo;
+    
+    const size_t expected = 100;
+    destructo.Reserve(expected);
+    for (size_t i = 0; i < expected; ++i)
+    {
+        // todo emplace?
+        destructo.Append(DestructoType());
+    }
 
-	// test that we're actually calling the dtors
-	destructo.Clear();
-	EXPECT_EQ(DestructoType::destruction_count, expected * 2); // *2 because the tmp gets destroyed as well, todo will this still happen if I make a move cons?
+    // test that we're actually calling the dtors
+    destructo.Clear();
+    EXPECT_EQ(DestructoType::destruction_count, expected * 2); // *2 because the tmp gets destroyed as well, todo will this still happen if I make a move cons?
 }
 
 TEST_F(MyVecTest, Grow)
 {
-	static const uint16_t growths = 16;
+    static const uint16_t growths = 16;
 
-	for (size_t i = 0; i < growths; ++i)
-	{
-		v.Grow();
-		EXPECT_EQ(v.Capacity(), std::pow(2, i));
-	}
+    for (size_t i = 0; i < growths; ++i)
+    {
+        v.Grow();
+        EXPECT_EQ(v.Capacity(), std::pow(2, i));
+    }
 
-	EXPECT_EQ(v.Size(), 0);
+    EXPECT_EQ(v.Size(), 0);
 }
 
 TEST_F(MyVecTest, Alignment)
 {
-	size_t requiredAlignment;
-	size_t misalignmentMask;
-	size_t misalignment;
+    size_t requiredAlignment;
+    size_t misalignmentMask;
+    size_t misalignment;
 
-	vc.Append('a');
-	requiredAlignment = alignof(char);
-	EXPECT_EQ(requiredAlignment, 1);
-	misalignmentMask = requiredAlignment - 1;
-	misalignment = reinterpret_cast<uintptr_t>(&vc[0]) & misalignmentMask;
-	EXPECT_EQ(misalignment, 0);
+    vc.Append('a');
+    requiredAlignment = alignof(char);
+    EXPECT_EQ(requiredAlignment, 1);
+    misalignmentMask = requiredAlignment - 1;
+    misalignment = reinterpret_cast<uintptr_t>(&vc[0]) & misalignmentMask;
+    EXPECT_EQ(misalignment, 0);
 
-	vf.Append(1.0f);
-	requiredAlignment = alignof(float);
-	EXPECT_EQ(requiredAlignment, 4);
-	misalignmentMask = requiredAlignment - 1;
-	misalignment = reinterpret_cast<uintptr_t>(&vf[0]) & misalignmentMask;
-	EXPECT_EQ(misalignment, 0);
+    vf.Append(1.0f);
+    requiredAlignment = alignof(float);
+    EXPECT_EQ(requiredAlignment, 4);
+    misalignmentMask = requiredAlignment - 1;
+    misalignment = reinterpret_cast<uintptr_t>(&vf[0]) & misalignmentMask;
+    EXPECT_EQ(misalignment, 0);
 
-	vd.Append(1.0);
-	requiredAlignment = alignof(double);
-	EXPECT_EQ(requiredAlignment, 8);
-	misalignmentMask = requiredAlignment - 1;
-	misalignment = reinterpret_cast<uintptr_t>(&vd[0]) & misalignmentMask;
-	EXPECT_EQ(misalignment, 0);
+    vd.Append(1.0);
+    requiredAlignment = alignof(double);
+    EXPECT_EQ(requiredAlignment, 8);
+    misalignmentMask = requiredAlignment - 1;
+    misalignment = reinterpret_cast<uintptr_t>(&vd[0]) & misalignmentMask;
+    EXPECT_EQ(misalignment, 0);
 
-	v3i.Append({ 0, 1, 2 });
-	requiredAlignment = alignof(ThreeIntType);
-	EXPECT_EQ(requiredAlignment, 4);
-	misalignmentMask = requiredAlignment - 1;
-	misalignment = reinterpret_cast<uintptr_t>(&v3i[0]) & misalignmentMask;
-	EXPECT_EQ(misalignment, 0);
+    v3i.Append({ 0, 1, 2 });
+    requiredAlignment = alignof(ThreeIntType);
+    EXPECT_EQ(requiredAlignment, 4);
+    misalignmentMask = requiredAlignment - 1;
+    misalignment = reinterpret_cast<uintptr_t>(&v3i[0]) & misalignmentMask;
+    EXPECT_EQ(misalignment, 0);
 
-	vp.Append({ 'a', 1.0 });
-	requiredAlignment = alignof(PaddedType);
-	EXPECT_EQ(requiredAlignment, 8);
-	misalignmentMask = requiredAlignment - 1;
-	misalignment = reinterpret_cast<uintptr_t>(&vp[0]) & misalignmentMask;
-	EXPECT_EQ(misalignment, 0);
+    vp.Append({ 'a', 1.0 });
+    requiredAlignment = alignof(PaddedType);
+    EXPECT_EQ(requiredAlignment, 8);
+    misalignmentMask = requiredAlignment - 1;
+    misalignment = reinterpret_cast<uintptr_t>(&vp[0]) & misalignmentMask;
+    EXPECT_EQ(misalignment, 0);
 
-	// personal type size knowledge checks
-	EXPECT_EQ(sizeof(char), 1);
-	EXPECT_EQ(sizeof(float), 4);
-	EXPECT_EQ(sizeof(double), 8);
-	EXPECT_EQ(sizeof(ThreeIntType), 12);
-	EXPECT_EQ(sizeof(PaddedType), 16);
-	EXPECT_EQ(sizeof(Pigeon), 1);
+    // personal type size knowledge checks
+    EXPECT_EQ(sizeof(char), 1);
+    EXPECT_EQ(sizeof(float), 4);
+    EXPECT_EQ(sizeof(double), 8);
+    EXPECT_EQ(sizeof(ThreeIntType), 12);
+    EXPECT_EQ(sizeof(PaddedType), 16);
+    EXPECT_EQ(sizeof(Pigeon), 1);
 }
 
 TEST_F(MyVecTest, CopyConstructionAndDestruction)
 {
-	static const size_t insertions = 16;
-	for (size_t i = 0; i < insertions; ++i)
-	{
-		vpigeons.Append(Pigeon{});
-	}
+    static const size_t insertions = 16;
+    for (size_t i = 0; i < insertions; ++i)
+    {
+        vpigeons.Append(Pigeon{});
+    }
 
-	EXPECT_EQ(PigeonManager::mPigeons.size(), insertions);
+    EXPECT_EQ(PigeonManager::mPigeons.size(), insertions);
 
-	Pigeon* firstPigeyAddr = &vpigeons[0];
+    Pigeon* firstPigeyAddr = &vpigeons[0];
 
-	EXPECT_TRUE(PigeonManager::IsRegistered(firstPigeyAddr));
-	vpigeons.Grow(); // should invalidate all pigeon pointers
-	EXPECT_FALSE(PigeonManager::IsRegistered(firstPigeyAddr));
+    EXPECT_TRUE(PigeonManager::IsRegistered(firstPigeyAddr));
+    vpigeons.Grow(); // should invalidate all pigeon pointers
+    EXPECT_FALSE(PigeonManager::IsRegistered(firstPigeyAddr));
 }
